2f3c456c336aad1fd77de7abb2e56e46300f1829
kaylalarussrll <kaylalarussell96@gmail.com>
2024-03-25 18:49:36 -0700

    add ability to randomly generate a sign and alter the object

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+//function to randomly generate arrays for each of the 3, and return one of the 12 zodiacs
+const returnRndmSign = () => {
+    const signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+    return signs[Math.floor(Math.random() * 12)];
+  }
 
 //object creation to store multitude of arrays
-let big3 = {
-    sun: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
-    moon: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
-    rising: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
-    
+const big3Generator = (number, sun, moon, rising) => {
+    return {
+        number:number,
+        sun: sun,
+        moon: moon,
+        rising: rising,
+
+    }
+
 }
+
+const ins1 = big3Generator(1, returnRndmSign(), returnRndmSign(), returnRndmSign());
+
+console.log(ins1);
