19b4c0f5f9ddef8a997a2ce6dc3f507676be7ab3
kaylalarussrll <kaylalarussell96@gmail.com>
2024-03-25 18:21:44 -0700

    create object and arrays

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-/*class Song {
-    constructor(title, artist) {
-      this.title = title;
-      this.artist = artist;
-    }
-  }*/
 
-  class BigThree {
-    constructor(sun, moon, rising) {
-        this.sun = sun;
-        this.moon = moon;
-        this.rising = rising;
-    }
-
-    get sun() {
-        return this.sun;
-    }
-
-    get moon() {
-        return this.moon;
-    }
-
-    get rising() {
-        return this.rising;
-    }
-
-  }
+//object creation to store multitude of arrays
+let big3 = {
+    sun: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    moon: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    rising: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    
+}
