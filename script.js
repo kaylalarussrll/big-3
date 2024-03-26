7349c9517558b539454a555c4c0a8a091cfd006f
kaylalarussrll <kaylalarussell96@gmail.com>
2024-03-25 17:57:21 -0700

    class creation

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello there, General Kenobi');
+/*class Song {
+    constructor(title, artist) {
+      this.title = title;
+      this.artist = artist;
+    }
+  }*/
+
+  class BigThree {
+    constructor(sun, moon, rising) {
+        this.sun = sun;
+        this.moon = moon;
+        this.rising = rising;
+    }
+
+    get sun() {
+        return this.sun;
+    }
+
+    get moon() {
+        return this.moon;
+    }
+
+    get rising() {
+        return this.rising;
+    }
+
+  }
