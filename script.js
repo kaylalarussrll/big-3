ffb63d91c88ff75edccc0f9b08c4992902cc4204
kaylalarussrll <kaylalarussell96@gmail.com>
2024-04-01 10:19:18 -0700

    add obj:phrases and populate

--- script.js
+++ script.js
@@ -11,11 +11,24 @@ const big3Generator = (number, sun, moon, rising) => {
         sun: sun,
         moon: moon,
         rising: rising,
-
     }
-
 }
+
+let phrases = {
+    'p1' : 'You will have good luck',
+    'p2' : 'You will have bad luck',
+    'p3' : 'You will come into a great fortune',
+    'p4' : 'Watch out when dealing with money',
+    'p5' : 'Do not trust easily in this time',
+    'p6' : 'Now is not the time for drastic change',
+    'p7' : 'Reconnect with old loved ones or friends',
+    'p8' : 'Gain perspective before moving forward',
+    'p9' : 'You will find what you are looking for'
+
+};
+
 
 const ins1 = big3Generator(1, returnRndmSign(), returnRndmSign(), returnRndmSign());
 
-console.log(ins1);
+//console.log(ins1);
+
