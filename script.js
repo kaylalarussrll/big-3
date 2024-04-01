02ca122c706074c3eb1a50cb1e9d9a3a3ec4a9a6
kaylalarussrll <kaylalarussell96@gmail.com>
2024-04-01 11:13:10 -0700

    added ability to pick random phrase and log it.

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const returnRndmSign = () => {
     return signs[Math.floor(Math.random() * 12)];
   }
 
+  const returnRndmPhrase = () => {
+    const phrases = ['This will bring you luck.', 
+    'This will guide you down the proper path.',
+    'Do not make any big decisions with money.',
+    'Avoid conflicts in your life.',
+    'Speak your mind in any upcoming situations that could arise.',
+    'Life will be what you make it.'];
+    return phrases[Math.floor(Math.random() * 6)];
+  }
+
 //object creation to store multitude of arrays
 const big3Generator = (number, sun, moon, rising) => {
     return {
@@ -11,21 +21,26 @@ const big3Generator = (number, sun, moon, rising) => {
         sun: sun,
         moon: moon,
         rising: rising,
-
+    
         buildMessage() {
             console.log(`Your sun sign is ${sun}, which is your identity. Your moon sign is ${moon}, which is your feelings and emotions. Your rising sign is ${rising}, which reflects physical appearance.`)
+        },
+        pickFortune() {
+            let fortune = returnRndmPhrase();
+            console.log(`Your big 3 would like to give you this fortune: ${fortune}`);
         }
+        
     }
-}
+};
 
-
-
+//for (let sun in big3Generator) {
+  //  console.log(`Your sun sign, ${sun} grants you this fortune: ${returnRndmPhrase}`);
+//}
 
 const ins1 = big3Generator(1, returnRndmSign(), returnRndmSign(), returnRndmSign());
 
 ins1.buildMessage();
-
-
+ins1.pickFortune();
 
 
 
